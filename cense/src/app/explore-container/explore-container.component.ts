a2eab4ca9e30d4afeb45aab7c86e8b1f5fe21d32
Alex Morehead <alex.morehead@gmail.com>
2020-05-21 17:31:08 -0500

    Design Home Page with Download/Upload Spreadsheet Buttons

--- cense/src/app/explore-container/explore-container.component.ts
+++ cense/src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-explore-container',
@@ -6,10 +8,61 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent implements OnInit {
-  @Input() name: string;
+  storeData: any;
+  csvData: any;
+  jsonData: any;
+  textData: any;
+  htmlData: any;
+  uploadedFile: File;
+  worksheet: any;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
+  storeUploadedFile(event: any) {
+    this.uploadedFile = event.target.files[0];
+    this.readXLSXFile();
+  }
+
+  readXLSXFile() {
+    let readFile = new FileReader();
+    readFile.onload = (e) => {
+      this.storeData = readFile.result;
+      var data = new Uint8Array(this.storeData);
+      var arr = new Array();
+      for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      debugger
+      var bstr = arr.join("");
+      var workbook = XLSX.read(bstr, { type: "binary" });
+      var first_sheet_name = workbook.SheetNames[0];
+      this.worksheet = workbook.Sheets[first_sheet_name];
+    }
+    readFile.readAsArrayBuffer(this.uploadedFile);
+  }
+
+  readAsCSV() {
+    this.csvData = XLSX.utils.sheet_to_csv(this.worksheet);
+    const data: Blob = new Blob([this.csvData], { type: 'text/csv;charset=utf-8;' });
+    FileSaver.saveAs(data, "CSVFile" + new Date().getTime() + '.csv');
+  }
+
+  readAsJson() {
+    this.jsonData = XLSX.utils.sheet_to_json(this.worksheet, { raw: false });
+    this.jsonData = JSON.stringify(this.jsonData);
+    const data: Blob = new Blob([this.jsonData], { type: "application/json" });
+    FileSaver.saveAs(data, "JsonFile" + new Date().getTime() + '.json');
+  }
+
+  readAsHTML() {
+    this.htmlData = XLSX.utils.sheet_to_html(this.worksheet);
+    const data: Blob = new Blob([this.htmlData], { type: "text/html;charset=utf-8;" });
+    FileSaver.saveAs(data, "HtmlFile" + new Date().getTime() + '.html');
+  }
+
+  readAsText() {
+    this.textData = XLSX.utils.sheet_to_txt(this.worksheet);
+    const data: Blob = new Blob([this.textData], { type: 'text/plain;charset=utf-8;' });
+    FileSaver.saveAs(data, "TextFile" + new Date().getTime() + '.txt');
+  }
 }
